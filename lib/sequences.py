c1373217159578752a6c27352604e5eac44c55e4
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-02-28 13:35:09 +0300

    Assignment completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1]
+    
+    while len(fibonacci_sequence) < length:
+        num = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(num)
+        
+    print(fibonacci_sequence[:length])
+   
+print_fibonacci(10)    
+pass
